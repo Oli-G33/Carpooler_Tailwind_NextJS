d370b9c85ef19b7c1ada5170eeb557fb395d1842
OliGarc333 <ngarcia333@gmail.com>
2023-12-10 14:18:26 +0100

    Styling changes to footer

--- client/app/components/Copyright.jsx
+++ client/app/components/Copyright.jsx
@@ -1,7 +1,6 @@
 'use client';
 import { useTheme } from 'next-themes';
-import Link from 'next/link';
-import React from 'react';
+import { FaGithub, FaLinkedin, FaEnvelope, FaWhatsapp } from 'react-icons/fa';
 
 const currentYear = new Date().getFullYear();
 
@@ -33,42 +32,23 @@ const Copyright = () => {
                   rel="noopener noreferrer"
                   className="hover:underline me-4 md:me-6"
                 >
-                  <svg
-                    className="w-6 h-6 text-gray-800 dark:text-white"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+                  <FaGithub
+                    size="25px"
+                    color={theme === 'dark' ? 'white' : '#1e3a8c'}
+                  />
                 </a>
               </li>
               <li className="ml-4">
                 <a
-                  href="https://www.linkedin.com/in/your-linkedin-profile"
+                  href="https://www.linkedin.com/in/oliver-garcia3"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="hover:underline me-4 md:me-6"
                 >
-                  <svg
-                    className="w-6 h-6 text-gray-800 dark:text-white"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 15 15"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M7.979 5v1.586a3.5 3.5 0 0 1 3.082-1.574C14.3 5.012 15 7.03 15 9.655V15h-3v-4.738c0-1.13-.229-2.584-1.995-2.584-1.713 0-2.005 1.23-2.005 2.5V15H5.009V5h2.97ZM3 2.487a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"
-                      clipRule="evenodd"
-                    />
-                    <path d="M3 5.012H0V15h3V5.012Z" />
-                  </svg>
+                  <FaLinkedin
+                    size="25px"
+                    color={theme === 'dark' ? 'white' : '#1e3a8c'}
+                  />
                 </a>
               </li>
               <li className="ml-4">
@@ -76,21 +56,10 @@ const Copyright = () => {
                   href={`mailto:${process.env.NEXT_PUBLIC_CONTACT_EMAIL}`}
                   className="hover:underline me-4 md:me-6"
                 >
-                  <svg
-                    className="w-6 h-6 text-gray-800 dark:text-white"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 12"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="m19 2-8.4 7.05a1 1 0 0 1-1.2 0L1 2m18 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1m18 0v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2"
-                    />
-                  </svg>
+                  <FaEnvelope
+                    size="25px"
+                    color={theme === 'dark' ? 'white' : '#1e3a8c'}
+                  />
                 </a>
               </li>
               <li className="ml-4">
@@ -100,15 +69,10 @@ const Copyright = () => {
                   href={`https://wa.me/${process.env.NEXT_PUBLIC_CONTACT_PHONE}`}
                   className="text-gray-800 hover:underline me-4 md:me-6 dark:text-white"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="22"
-                    height="22"
-                    fill="currentColor"
-                  >
-                    <path d="M0.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z" />
-                  </svg>
+                  <FaWhatsapp
+                    size="25px"
+                    color={theme === 'dark' ? 'white' : '#1e3a8c'}
+                  />
                 </a>
               </li>
             </ul>
